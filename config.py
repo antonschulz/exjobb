29cf5e05d5779f8b0486adac49ab5ab0a6d46943
antonschulz <anton.ph.schulz@gmail.com>
2025-04-17 10:21:56 +0200

    configs

--- config.py
+++ config.py
@@ -12,15 +12,15 @@ def sample_lstm_config():
     config = {
         "input_size": 2,
         "num_classes": 4,
-        "hidden_size": 64,
+        "hidden_size": random.choice([64, 32, 16]),
         "num_layers": random.choice([2,4,8]),
-        "fc_units": [32],
+        "fc_units": random.choice([[32], [32, 16]]),
         "bi_lstm": False,
-        "dropout_lstm": 0.0,
-        "dropout_fc": 0.0,
-        "num_epochs": 50,
-        "learning_rate": 1e-3,
-        "weight_decay": 0.0,
+        "dropout_lstm": random.choice([0.0, 0.1]),
+        "dropout_fc": random.choice([0.0,0.1, 0.2]),
+        "num_epochs": random.choice([100,300]),
+        "learning_rate": random.choice([1e-3, 1e-4, 5e-4]),
+        "weight_decay": random.choice([0.0, 1e-5]),
     }
     return config
 
@@ -49,7 +49,7 @@ def sample_tcn_config():
         "num_levels": random.choice([3, 6, 9, 12, 20]),
         "kernel_size": random.choice([1,3,5,10]),
         "dropout": random.choice([0, 0.3]),
-        "num_filters": 16,
+        "num_filters": random.choice([8,16,32]),
         "weight_decay": random.choice([0,1e-5]),
     }
     return config
