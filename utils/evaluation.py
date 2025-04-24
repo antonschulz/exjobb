8d76310dd77a0904a02e76615a31f378b0937cf2
antonschulz <anton.ph.schulz@gmail.com>
2025-04-24 14:40:35 +0200

    confusion matrix

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, hamming_loss
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, hamming_loss, classification_report,  confusion_matrix
 
 def evaluate_model(model, val_data):
     """
@@ -29,6 +29,25 @@ def evaluate_model(model, val_data):
     metrics["precision_macro"] = precision_score(true_labels, predictions, average='macro', zero_division=0)
     metrics["recall_macro"] = recall_score(true_labels, predictions, average='macro', zero_division=0)
     metrics["f1_macro"] = f1_score(true_labels, predictions, average='macro', zero_division=0)
+
+        # Generate the text report
+    report = classification_report(true_labels, predictions, zero_division=0, output_dict=True)
+    metrics["classification_report"] = report
+
+     # confusion matrix as nested dict
+    # determine sorted labels
+    labels = sorted(set(true_labels) | set(predictions))
+    cm = confusion_matrix(true_labels, predictions, labels=labels)
+    cm_dict = {
+        int(true_label): {
+            int(pred_label): int(cm[i, j])
+            for j, pred_label in enumerate(labels)
+        }
+        for i, true_label in enumerate(labels)
+    }
+    metrics["confusion_matrix"] = cm_dict
+
+
     
     # Optionally, you can also compute additional metrics here.
     return metrics
